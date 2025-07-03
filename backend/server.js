f43873f65e41c619608f98d8d3c9e1110e572f94
Varshil13 <sharmapriyanshu91@gmail.com>
2025-07-04 04:41:48 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+// backend/server.js
+const express = require('express');
+const cors = require('cors');
+const quizRoutes = require("./routes/quizRoutes.js")
+const connectToDatabase = require('./connect');
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/authRoutes.js");
+
+require('dotenv').config();
+const app = express();
+
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api", quizRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+
+connectToDatabase(process.env.MONGO_URI).then(() => {
+  console.log('MongoDB connected');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
