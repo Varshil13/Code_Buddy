2c4557c77c4d0f983d6b134d97fb491a1404035d
Varshil13 <sharmapriyanshu91@gmail.com>
2025-07-04 05:13:03 +0530

    Cors Frontend Url Updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://code-buddy-frontend.onrender.com"
+  ],
   credentials: true,
 }));
 app.use(express.json());
